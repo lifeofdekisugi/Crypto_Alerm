c531de4960c188e27d1c571613f0c248b299e348
Shahir Islam <lifeofdekisugi@gmail.com>
2022-05-12 03:53:14 +0600

    Load More (!)

--- app/src/main/java/com/shahir/cryptoalerm/MainFragments/Dashboard/FragmentDashboard.java
+++ app/src/main/java/com/shahir/cryptoalerm/MainFragments/Dashboard/FragmentDashboard.java
@@ -9,11 +9,15 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -36,6 +40,13 @@ public class FragmentDashboard extends Fragment {
 
     private List<TokenPriseItem> tokenPriseItems;
     private RecyclerView.Adapter tokenPriceAdapter;
+    LinearLayoutManager manager;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    Boolean isScrolling = false;
+    int currentItems, totalItems, scrolledOutItems;
+
+    int pageNumber = 1;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -50,21 +61,70 @@ public class FragmentDashboard extends Fragment {
         //View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
 
+
         getCoinData();
 
         tokenPriseItems =new ArrayList<>();
 
-        LinearLayoutManager manager = new LinearLayoutManager(mContext);
+        manager = new LinearLayoutManager(mContext);
         binding.tokenPriseRecyclerView.setLayoutManager(manager);
         tokenPriceAdapter = new TokenPriseAdapted(tokenPriseItems,mContext,this);
         binding.tokenPriseRecyclerView.setAdapter(tokenPriceAdapter);
 
+        binding.swipeToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getCoinData();
+
+                binding.swipeToRefresh.setRefreshing(false);
+            }
+        });
+
+        binding.tokenPriseRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+//                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+//                    isScrolling = true;
+//                }
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+
+
+                currentItems = manager.getChildCount();
+                totalItems = manager.getItemCount();
+                scrolledOutItems = manager.findFirstVisibleItemPosition();
+
+                if (currentItems + totalItems == totalItems){
+                    Toast.makeText(mContext, "Completed", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
         return binding.getRoot();
     }
 
+    private void loadMoreData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pageNumber = pageNumber + 1;
+
+                Log.d(TAG, "run: @@@@@@@@@@@@              Page Number : " + pageNumber);
+            }
+        }, 2000);
+    }
+
     public void getCoinData(){
+
         RequestQueue queue = Volley.newRequestQueue(mContext);
-        String url = "https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd";
+        String url = "https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd&&page=1";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new com.android.volley.Response.Listener<JSONObject>() {
             @Override
@@ -97,10 +157,9 @@ public class FragmentDashboard extends Fragment {
 
                         Log.d(TAG, "onResponse: @@@@@@@@@@             Token name : " + mainObject.getString("symbol") + " || Token Price : " +
                                 market_data.getString("price_usd"));
-
                     }
-                    tokenPriceAdapter.notifyDataSetChanged();
 
+                    tokenPriceAdapter.notifyDataSetChanged();
 
                 } catch (JSONException e) {
                     Log.d(TAG, "onResponse: @@@@@@@@@@            Object Error : " + e);
